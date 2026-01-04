0f382f070fbf88d6e011433cc92f9880d35f4357
eliekoutoua898-afk <eliekoutoua898@gmail.com>
2026-01-04 23:58:52 +0000

    Update setting.js

--- setting.js
+++ setting.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263776388689', // add your phone number
+  OWNER_NAME: 'loelys23, //your name
+  OWNER_NUMBER: '2250160083311', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare', // ur timezone
